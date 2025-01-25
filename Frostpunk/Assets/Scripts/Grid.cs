edde2ccfd64bad6b005f46e69536cb47b8595b94
AndrewZ <andrewzemetskiy@gmail.com>
2025-01-25 13:00:35 +0300

    add: set values to grid and set new origin

--- Frostpunk/Assets/Scripts/Grid.cs
+++ Frostpunk/Assets/Scripts/Grid.cs
@@ -6,17 +6,19 @@ public class Grid
     private int _width;
     private int _height;
     private float _cellSize;
+    private Vector3 _originPosition;
     private int[,] _gridArray;
-    private TextMesh[,] _debugTextArray;
+    private TextMesh[,] _valueTextArray;
 
-    public Grid(int width, int height, float cellSize)
+    public Grid(int width, int height, float cellSize, Vector3 originPosition)
     {
         _width = width;
         _height = height;
         _cellSize = cellSize;
+        _originPosition = originPosition;
 
         _gridArray = new int[_width, _height];
-        _debugTextArray = new TextMesh[_width, _height];
+        _valueTextArray = new TextMesh[_width, _height];
 
         for (int x = 0; x < _gridArray.GetLength(0); x++)
         {
@@ -27,7 +29,7 @@ public class Grid
                     Color.white, TextAnchor.MiddleCenter);
 
                 text.transform.eulerAngles = new Vector3(90f, 0f, 0f);
-                _debugTextArray[x, y] = text;
+                _valueTextArray[x, y] = text;
 
                 Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
                 Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
@@ -36,27 +38,25 @@ public class Grid
 
         Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
         Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
-
-        SetValue(2, 1, 56);
     }
 
     private Vector3 GetWorldPosition(int x, int z)
     {
-        return new Vector3(x, 0, z) * _cellSize;
+        return new Vector3(x, 0, z) * _cellSize + _originPosition;
     }
 
     private void GetXZ(Vector3 worldPosition, out int x, out int z)
     {
-        x = Mathf.FloorToInt(worldPosition.x / _cellSize);
-        z = Mathf.FloorToInt(worldPosition.z / _cellSize);
+        x = Mathf.FloorToInt((worldPosition - _originPosition).x / _cellSize);
+        z = Mathf.FloorToInt((worldPosition - _originPosition).z / _cellSize);
     }
-    
+
     public void SetValue(int x, int z, int value)
     {
         if (0 <= x && 0 <= z && x < _width && z < _height)
         {
             _gridArray[x, z] = value;
-            _debugTextArray[x, z].text = _gridArray[x, z].ToString();
+            _valueTextArray[x, z].text = _gridArray[x, z].ToString();
         }
     }
 
@@ -65,5 +65,24 @@ public class Grid
         int x, z;
         GetXZ(worldPosition, out x, out z);
         SetValue(x, z, value);
+    }
+
+    public int GetValue(int x, int z)
+    {
+        if (0 <= x && 0 <= z && x < _width && z < _height)
+        {
+            return _gridArray[x, z];
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public int GetValue(Vector3 worldPosition)
+    {
+        int x, z;
+        GetXZ(worldPosition, out x, out z);
+        return GetValue(x, z);
     }
 }
--- Frostpunk/Assets/Scripts/Test.cs
+++ Frostpunk/Assets/Scripts/Test.cs
@@ -6,23 +6,34 @@ public class Test : MonoBehaviour
 {
     [SerializeField] private Camera _cam;
     [SerializeField] private LayerMask _layerMask;
-
+    [SerializeField] private Transform _gridOrigin;
+    
     private Grid _grid;
 
     private void Start()
     {
-        _grid = new Grid(5, 10, 4f);
+        _grid = new Grid(5, 10, 4f, _gridOrigin.position);
     }
 
     private void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            // _grid.SetValue(UtilsClass.GetMouseWorldPositionWithZ(), 50);
             Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, _layerMask))
             {
-                _grid.SetValue(raycastHit.point, 50);
+                var value = _grid.GetValue(raycastHit.point);
+                _grid.SetValue(raycastHit.point, value + 1);
+                transform.position = raycastHit.point;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, _layerMask))
+            {
+                Debug.Log(_grid.GetValue(raycastHit.point));
                 transform.position = raycastHit.point;
             }
         }
