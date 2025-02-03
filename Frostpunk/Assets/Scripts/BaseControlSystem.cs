f9d44f46592425d8e9a9543d185a774ef23ddbb7
AndrewZ <andrewzemetskiy@gmail.com>
2025-02-03 23:20:27 +0300

    add: basic setActive for resourceUI

--- Frostpunk/Assets/Scripts/BaseControlSystem.cs
+++ Frostpunk/Assets/Scripts/BaseControlSystem.cs
@@ -7,15 +7,18 @@ using UnityEngine.InputSystem;
 public class BaseControlSystem : MonoBehaviour
 {
     public static BaseControlSystem Instance;
-    
-    [Header("Residents")]
-    [SerializeField] private List<NavMeshAgent> _navMeshAgentList;
-    [Header("Buildings")]
-    [SerializeField] private List<Building> _buildingList;
-    
+
+    [Header("Residents")] [SerializeField] private List<NavMeshAgent> _navMeshAgentList;
+    [Header("Buildings")] [SerializeField] private List<Building> _buildingList;
+
+    [Header("UI")] [SerializeField] private GameObject _resourceUI;
+
     private InputSystem _inputSystem;
     private Camera _camera;
     private float _baseSpeed;
+
+    //For UI
+    private bool isResourceUIActive = false;
 
     private void Awake()
     {
@@ -39,18 +42,25 @@ public class BaseControlSystem : MonoBehaviour
         _inputSystem.Disable();
         _inputSystem.Player.Move.performed -= OnMouseClick;
     }
-    
+
     private void OnMouseClick(InputAction.CallbackContext obj)
     {
         Ray ray = _camera.ScreenPointToRay(Mouse.current.position.value);
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
-            if(hit.collider.TryGetComponent<Building>(out Building building))
+            if (hit.collider.TryGetComponent<Building>(out Building building))
             {
                 Debug.Log(building.BuildingName);
+                if (isResourceUIActive == false)
+                {
+                    SetEnableToGO(_resourceUI, true);
+                }
+            }
+            else if (isResourceUIActive == true)
+            {
+                SetEnableToGO(_resourceUI, false);
             }
             
-            // _navMeshAgent.destination = hit.point;
         }
     }
 
@@ -62,5 +72,12 @@ public class BaseControlSystem : MonoBehaviour
         {
             _navMeshAgentList[residentId].SetDestination(_buildingList[buildingId].TargetPoint.position);
         }
+    }
+
+    //UI
+    private void SetEnableToGO(GameObject target, bool isEnable)
+    {
+        target.SetActive(isEnable);
+        isResourceUIActive = isEnable;
     }
 }
