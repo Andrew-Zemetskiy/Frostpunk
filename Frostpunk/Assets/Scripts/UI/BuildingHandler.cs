ee4cf02ccd8b526a28bf43e6883b577eaabd767c
AndrewZ <andrewzemetskiy@gmail.com>
2025-02-08 12:30:48 +0300

    add: building UI and cost requirements

--- Frostpunk/Assets/Scripts/UI/BuildingHandler.cs
+++ Frostpunk/Assets/Scripts/UI/BuildingHandler.cs
@@ -1,10 +1,14 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BuildingHandler : UIHandlerBase, IInit
 {
     public static BuildingHandler Instance;
 
+    [SerializeField] private List<BuildingCost> _buildingsCost;
+    
     public event Action<BuildingType> OnBuildingSelected; 
     
     private void Awake()
@@ -14,13 +18,46 @@ public class BuildingHandler : UIHandlerBase, IInit
 
     public override void Init()
     {
-       
+        InGameTimeManager.Instance.OnTimeStep += CheckCostRequirement;
     }
 
     public void SelectBuildingTypeByIndex(int index)
     {
+        if (_buildingsCost.Count <= index) return;
+
         BuildingType[] values = (BuildingType[])Enum.GetValues(typeof(BuildingType));
         if (values.Length <= index) return;
         OnBuildingSelected?.Invoke(values[index]);
     }
+
+    private void CheckCostRequirement()
+    {
+        float coal, wood, steel;
+        coal = ResourceHandler.Instance.CoalAmount;
+        wood = ResourceHandler.Instance.WoodAmount;
+        steel = ResourceHandler.Instance.SteelAmount;
+
+        bool isFit;
+        foreach (var building in _buildingsCost)
+        {
+            isFit = true;
+            if (building.coal > coal || building.wood > wood || building.steel > steel)
+            {
+                isFit = false;
+            }
+            
+            building.button.interactable = isFit;
+        }
+    }
+    
+}
+
+[System.Serializable]
+public struct BuildingCost
+{
+    public BuildingType type;
+    public Button button;
+    public int coal;
+    public int wood;
+    public int steel;
 }
