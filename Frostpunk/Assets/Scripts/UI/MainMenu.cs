002669420d577af6c4636921848002a71dd0da3d
AndrewZ <andrewzemetskiy@gmail.com>
2025-02-08 12:49:17 +0300

    add: loading screen

--- Frostpunk/Assets/Scripts/UI/MainMenu.cs
+++ Frostpunk/Assets/Scripts/UI/MainMenu.cs
@@ -1,13 +1,27 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject LoadingScreen;
+    
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        LoadingScreen.SetActive(true);
+        StartCoroutine(LoadLevelAsync());
     }
 
+    private IEnumerator LoadLevelAsync()
+    {
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+
+        while (!loadOperation.isDone)
+        {
+            yield return null;
+        }
+    }
+    
     public void ExitGame()
     {
         Application.Quit();
